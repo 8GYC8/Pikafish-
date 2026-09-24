0a9335014ff11eddb47afa30a428742da8f4197c
8GYC8 <109010807+8GYC8@users.noreply.github.com>
2026-09-24 21:31:07 +0800

    Update ucioption.cpp

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -192,17 +192,29 @@ std::ostream& operator<<(std::ostream& os, const OptionsMap& om) {
                 const Option& o = it.second;
                 os << "\noption name " << it.first << " type " << o.type;
 
-                if (o.type == "check" || o.type == "combo")
-                    os << " default " << o.defaultValue;
+                // TOML is loaded before the GUI sends `uci`, so advertise the
+                // configured current value as this process' startup default. This makes
+                // GUIs reflect pikafish.toml instead of the compile-time defaults.
+                if (o.type == "check")
+                    os << " default " << o.currentValue;
+
+                else if (o.type == "combo")
+                {
+                    os << " default " << o.currentValue;
+                    // defaultValue also stores the supported `var ...` suffix.
+                    const auto vars = o.defaultValue.find(" var ");
+                    if (vars != std::string::npos)
+                        os << o.defaultValue.substr(vars);
+                }
 
                 else if (o.type == "string")
                 {
-                    std::string defaultValue = o.defaultValue.empty() ? "<empty>" : o.defaultValue;
-                    os << " default " << defaultValue;
+                    std::string currentValue = o.currentValue.empty() ? "<empty>" : o.currentValue;
+                    os << " default " << currentValue;
                 }
 
                 else if (o.type == "spin")
-                    os << " default " << stoi(o.defaultValue) << " min " << o.min << " max "
+                    os << " default " << stoi(o.currentValue) << " min " << o.min << " max "
                        << o.max;
 
                 break;

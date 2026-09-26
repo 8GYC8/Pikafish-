3126bfbc1731e5bccc401027e7d01e6bfebd9643
8GYC8 <109010807+8GYC8@users.noreply.github.com>
2026-09-26 22:18:05 +0800

    Update uci.h

--- src/uci.h
+++ src/uci.h
@@ -54,7 +54,7 @@ class UCIEngine {
     static std::string to_lower(std::string str);
     static Move        to_move(const Position& pos, std::string str);
 
-    static inline ScoreTypeMode scoreTypeMode = ScoreTypeMode::PAWN_VALUE_NORMALIZED;
+    static inline ScoreTypeMode scoreTypeMode = ScoreTypeMode::Elo;
 
     Search::LimitsType parse_limits(std::istream& is);
 
